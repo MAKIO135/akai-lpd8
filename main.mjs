51b712b76ec10609fe1630393f9b2d7337626e46
makio135 <radisson@MAC22219-3.local>
2018-09-25 23:00:27 +0200

    notes on modules import url

--- main.mjs
+++ main.mjs
@@ -1,11 +1,13 @@
-import { AkaiLPD8 } from './modules/AkaiLPD8.mjs';
-
+// note for modules import url
+// <script type="module" src="./main.mjs?test=yo"></script>
 // console.log( ( new URL( import.meta.url ) ).searchParams.get( 'test' ) );
+
+import { AkaiLPD8 } from './modules/AkaiLPD8.mjs';
 
 const LPD8 = new AkaiLPD8( { log: true } );
 
 if( LPD8.init() ) {
-    LPD8.K1.on( 'change', () => console.log( LPD8.K1.velocity ) );
+    LPD8.K1.on( 'change', vel => console.log( vel ) );
 }
 
 console.log( LPD8 );
